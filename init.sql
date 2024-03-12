b29a74dbbe0e27efe7205be57f53fb012e3aaaa4
Taísso Reni <taissoreni@gmail.com>
2024-03-12 09:57:14 -0300

    adjust sql

--- init.sql
+++ init.sql
@@ -1,14 +1,14 @@
-CREATE TABLE
-  orders (
-    id varchar(255) NOT NULL,
-    price float NOT NULL,
-    tax float NOT NULL,
-    final_price float NOT NULL,
-    PRIMARY KEY (id)
-  );
+CREATE TABLE orders (
+  id varchar(255) NOT NULL,
+  price float NOT NULL,
+  tax float NOT NULL,
+  final_price float NOT NULL,
+  PRIMARY KEY (id)
+);
 
 INSERT INTO
   orders (id, price, tax, final_price)
 VALUES
-  ("1", 10.99, 0.5, 54.99)
-  ("2", 9.99, 0.5, 49.99);
+  ('1', 100.00, 10.00, 110.00),
+  ('2', 75.50, 7.55, 83.05),
+  ('3', 50.25, 5.03, 55.28);
