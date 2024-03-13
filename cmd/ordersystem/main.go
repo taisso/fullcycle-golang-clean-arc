bccfd614c821b6976e0423aea0b70314285608b4
Taísso Reni <taissoreni@gmail.com>
2024-03-13 09:00:55 -0300

    adjust method http

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -48,7 +48,7 @@ func main() {
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
 	webserver.AddHandler("POST /order", webOrderHandler.Create)
-	webserver.AddHandler("/order", webOrderHandler.FindAll)
+	webserver.AddHandler("GET /order", webOrderHandler.FindAll)
 
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
